7fbdbed26be906796758e561b755c738dad286d4
alexmathew98 <alexmathewdev2099@gmail.com>
2024-06-23 19:27:50 -0400

    Adding skeleton code

--- competitor_analysis_agents.py
+++ competitor_analysis_agents.py
@@ -10,7 +10,7 @@ from langchain.tools.yahoo_finance_news import YahooFinanceNewsTool
 
 # Test
 import os
-os.environ["OPENAI_API_KEY"] = "sk-proj-teEaMucAlXCFs2vbaWTHT3BlbkFJadJSIYWF3lFP4SjNGeqM"
+os.environ["OPENAI_API_KEY"] = "sk-proj-teEaMucAlXCFs2vbaWTHT3BlbkFJadJSIYWF3lFP4SjNGeqM"   #Need to hide API keys from here
 os.environ['OPENAI_MODEL_NAME'] = 'gpt-3.5-turbo-0125'
 
 class StockAnalysisAgents():
@@ -33,43 +33,43 @@ class StockAnalysisAgents():
       ]
     )
 
-  # Agent-2: Processing Agent
-  def processing_agent(self):
-    return Agent(
-      role='Staff Research Analyst',
-      goal="""Being the best at gather, interpret data and amaze
-      your customer with it""",
-      backstory="""Known as the BEST research analyst, you're
-      skilled in sifting through news, company announcements, 
-      and market sentiments. Now you're working on a super 
-      important customer""",
-      verbose=True,
-      tools=[
-        BrowserTools.scrape_and_summarize_website,
-        SearchTools.search_internet,
-        SearchTools.search_news,
-        YahooFinanceNewsTool(),
-        SECTools.search_10q,
-        SECTools.search_10k
-      ]
-    )
-
-  # Agent-3: Reporting Agent
-  def reporting_agent(self):
-    return Agent(
-      role='Private Investment Advisor',
-      goal="""Impress your customers with full analyses over stocks
-      and completer investment recommendations""",
-      backstory="""You're the most experienced investment advisor
-      and you combine various analytical insights to formulate
-      strategic investment advice. You are now working for
-      a super important customer you need to impress.""",
-      verbose=True,
-      tools=[
-        BrowserTools.scrape_and_summarize_website,
-        SearchTools.search_internet,
-        SearchTools.search_news,
-        CalculatorTools.calculate,
-        YahooFinanceNewsTool()
-      ]
-    )
+  # # Agent-2: Processing Agent
+  # def processing_agent(self):
+  #   return Agent(
+  #     role='Staff Research Analyst',
+  #     goal="""Being the best at gather, interpret data and amaze
+  #     your customer with it""",
+  #     backstory="""Known as the BEST research analyst, you're
+  #     skilled in sifting through news, company announcements,
+  #     and market sentiments. Now you're working on a super
+  #     important customer""",
+  #     verbose=True,
+  #     tools=[
+  #       BrowserTools.scrape_and_summarize_website,
+  #       SearchTools.search_internet,
+  #       SearchTools.search_news,
+  #       YahooFinanceNewsTool(),
+  #       SECTools.search_10q,
+  #       SECTools.search_10k
+  #     ]
+  #   )
+  #
+  # # Agent-3: Reporting Agent
+  # def reporting_agent(self):
+  #   return Agent(
+  #     role='Private Investment Advisor',
+  #     goal="""Impress your customers with full analyses over stocks
+  #     and completer investment recommendations""",
+  #     backstory="""You're the most experienced investment advisor
+  #     and you combine various analytical insights to formulate
+  #     strategic investment advice. You are now working for
+  #     a super important customer you need to impress.""",
+  #     verbose=True,
+  #     tools=[
+  #       BrowserTools.scrape_and_summarize_website,
+  #       SearchTools.search_internet,
+  #       SearchTools.search_news,
+  #       CalculatorTools.calculate,
+  #       YahooFinanceNewsTool()
+  #     ]
+  #   )
--- competitor_analysis_tasks.py
+++ competitor_analysis_tasks.py
@@ -27,74 +27,75 @@ class StockAnalysisTasks():
       agent=agent,
       expected_output='A refined finalized summary of the selected stock based on the details provided in the description'
     )
-  # Task-2
-  def financial_analysis(self, agent):
-    return Task(description=dedent(f"""
-        Conduct a thorough analysis of the stock's financial
-        health and market performance. 
-        This includes examining key financial metrics such as
-        P/E ratio, EPS growth, revenue trends, and 
-        debt-to-equity ratio. 
-        Also, analyze the stock's performance in comparison 
-        to its industry peers and overall market trends.
+  # # Task-2
+  # def financial_analysis(self, agent):
+  #   return Task(description=dedent(f"""
+  #       Conduct a thorough analysis of the stock's financial
+  #       health and market performance.
+  #       This includes examining key financial metrics such as
+  #       P/E ratio, EPS growth, revenue trends, and
+  #       debt-to-equity ratio.
+  #       Also, analyze the stock's performance in comparison
+  #       to its industry peers and overall market trends.
+  #
+  #       Your final report MUST expand on the summary provided
+  #       but now including a clear assessment of the stock's
+  #       financial standing, its strengths and weaknesses,
+  #       and how it fares against its competitors in the current
+  #       market scenario.{self.__tip_section()}
+  #
+  #       Make sure to use the most recent data possible.
+  #     """),
+  #     agent=agent,
+  #     expected_output='A refined finalized analysis of the mentioned stock based on the details provided in the description'
+  #     )
+  #
+  # # Task-3
+  # def filings_analysis(self, agent):
+  #   return Task(description=dedent(f"""
+  #       Analyze the latest 10-Q and 10-K filings from EDGAR for
+  #       the stock in question.
+  #       Focus on key sections like Management's Discussion and
+  #       Analysis, financial statements, insider trading activity,
+  #       and any disclosed risks.
+  #       Extract relevant data and insights that could influence
+  #       the stock's future performance.
+  #
+  #       Your final answer must be an expanded report that now
+  #       also highlights significant findings from these filings,
+  #       including any red flags or positive indicators for
+  #       your customer.
+  #       {self.__tip_section()}
+  #     """),
+  #     agent=agent,
+  #     expected_output='A refined finalized analysis of the mentioned stock based on the details provided in the description'
+  #     )
+  #
+  # # Task-4
+  # def recommend(self, agent):
+  #   return Task(description=dedent(f"""
+  #       Review and synthesize the analyses provided by the
+  #       Financial Analyst and the Research Analyst.
+  #       Combine these insights to form a comprehensive
+  #       investment recommendation.
+  #
+  #       You MUST Consider all aspects, including financial
+  #       health, market sentiment, and qualitative data from
+  #       EDGAR filings.
+  #
+  #       Make sure to include a section that shows insider
+  #       trading activity, and upcoming events like earnings.
+  #
+  #       Your final answer MUST be a recommendation for your
+  #       customer. It should be a full super detailed report, providing a
+  #       clear investment stance and strategy with supporting evidence.
+  #       Make it pretty and well formatted for your customer.
+  #       {self.__tip_section()}
+  #     """),
+  #     agent=agent,
+  #     expected_output='A refined list of recommendtiones similiar to the mentioned stock based on the details provided in the description'
+  #   )
 
-        Your final report MUST expand on the summary provided
-        but now including a clear assessment of the stock's
-        financial standing, its strengths and weaknesses, 
-        and how it fares against its competitors in the current
-        market scenario.{self.__tip_section()}
+  # def __tip_section(self):
+  #   return "If you do your BEST WORK, I'll give you a $10,000 commission!"                 //TODO: Need to modify this
 
-        Make sure to use the most recent data possible.
-      """),
-      agent=agent,
-      expected_output='A refined finalized analysis of the mentioned stock based on the details provided in the description'
-      )
-
-  # Task-3
-  def filings_analysis(self, agent):
-    return Task(description=dedent(f"""
-        Analyze the latest 10-Q and 10-K filings from EDGAR for
-        the stock in question. 
-        Focus on key sections like Management's Discussion and
-        Analysis, financial statements, insider trading activity, 
-        and any disclosed risks.
-        Extract relevant data and insights that could influence
-        the stock's future performance.
-
-        Your final answer must be an expanded report that now
-        also highlights significant findings from these filings,
-        including any red flags or positive indicators for
-        your customer.
-        {self.__tip_section()}        
-      """),
-      agent=agent,
-      expected_output='A refined finalized analysis of the mentioned stock based on the details provided in the description'
-      )
-
-  # Task-4
-  def recommend(self, agent):
-    return Task(description=dedent(f"""
-        Review and synthesize the analyses provided by the
-        Financial Analyst and the Research Analyst.
-        Combine these insights to form a comprehensive
-        investment recommendation. 
-        
-        You MUST Consider all aspects, including financial
-        health, market sentiment, and qualitative data from
-        EDGAR filings.
-
-        Make sure to include a section that shows insider 
-        trading activity, and upcoming events like earnings.
-
-        Your final answer MUST be a recommendation for your
-        customer. It should be a full super detailed report, providing a 
-        clear investment stance and strategy with supporting evidence.
-        Make it pretty and well formatted for your customer.
-        {self.__tip_section()}
-      """),
-      agent=agent,
-      expected_output='A refined list of recommendtiones similiar to the mentioned stock based on the details provided in the description'
-    )
-
-  def __tip_section(self):
-    return "If you do your BEST WORK, I'll give you a $10,000 commission!"
